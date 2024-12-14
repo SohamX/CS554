ffb41d96e3077458e46143682456b4e5ce9c8103
SohamX <sohamtalekar7@gmail.com>
2024-12-14 13:41:14 -0500

    css for search bar

--- Client/src/components/Student/Student.jsx
+++ Client/src/components/Student/Student.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../../contexts/AccountContext";
 import { AppBar, Toolbar, Typography, Button, Box, IconButton, InputBase, Card, CardContent, CardMedia } from '@mui/material';
 import backgroundImage from './../../assets/Background_1.png';
 import { Link } from 'react-router-dom';
-import { TextField, Container, Grid2 } from "@mui/material";
+import { TextField, Container, Grid2, Select } from "@mui/material";
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
 import axios from "axios";
@@ -110,97 +110,129 @@ const student = () => {
 
   return (
     <>
-      <div style={{ width: '100%', alignContent: 'center' }}>
-        <div>
-          <label>
+      <Container maxWidth="md" style={{ marginTop: "3%", marginBottom: "4%" }}>
+        <Box sx={{ mt: 4, p: 3, boxShadow: 3, borderRadius: 2 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
             Search For The Food That You Like...!
-          </label>
+          </Typography>
+          <Box mb={4} style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignContent: 'space-around',
+            justifyContent: 'space-between'
+          }}>
+            <TextField
+              id="nameValue"
+              margin="normal"
+              defaultValue={null}
+              placeholder='Food name...'
+              width={{width: 'auto'}}
+            />
+            <Select
+              id="cuisineValue"
+              margin="normal"
+              value={searchName || ''}
+              className='form-control'
+              onChange={(e) => {
+                setName(e.target.value)
+              }}
+              style={{
+                width: '20%',
+                height: '90%',
+                top: '1rem',
+              }}
+            >
+              <option value={''} disabled>Select Cuisine</option>
+              {cuisinesList &&
+                cuisinesList.map((cuisine) => {
+                  return (
+                    <option key={cuisine} value={cuisine}>
+                      {cuisine}
+                    </option>
+                  );
+                })}
+            </Select>
+            <TextField
+              id="locationValue"
+              margin="normal"
+              defaultValue={null}
+              placeholder='Location...'
+              style={{width: 'auto'}}
+            />
+            <br />
+            <TextField
+              id="minPriceValue"
+              margin="normal"
+              defaultValue={null}
+              placeholder='Min price...'
+              type="number" step="0.01"
+              style={{width: "20%"}}
+            />
+            <TextField
+              id="maxPriceValue"
+              margin="normal"
+              defaultValue={null}
+              placeholder='Max price...'
+              type="number" step="0.01"
+              style={{width: "20%"}}
+            />
+          </Box>
+          <Box mb={4}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={clearFields}
+              sx={{ mt: 2 }}
+            >
+              Clear Search
+            </Button>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={getData}
+              sx={{ mt: 2 }}
+            >
+              Search
+            </Button>
+          </Box>
+      </Box>
+      </Container>
+      {dishes && dishes.length > 0 && (
+        <div style={{ width: '100%', alignContent: 'center' }}>
+          <Grid2 container spacing={2}>
+            {dishes.map((dish) => (
+              <Link to={`/student/dishes/${dish._id}`}>
+                <Grid2 dish key={dish._id}>
+                  <Card>
+                    {/* <CardMedia
+                    component="img"
+                    height="140"
+                    image={item.image}
+                    alt={item.title}
+                  /> */}
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {dish.name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {dish.description}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        CUISINE: {dish.cuisineType}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        COST: {dish.cost} $
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid2>
+              </Link>
+            ))}
+          </Grid2>
           <br />
-          <input
-            id="nameValue"
-            defaultValue={null}
-            placeholder='Food name...'
-          />
-          <select
-            id="cuisineValue"
-            value={searchName || ''}
-            className='form-control'
-            onChange={(e) => {
-              setName(e.target.value)
-            }}
-          >
-            <option value={''} disabled>Select Cuisine</option>
-            {cuisinesList &&
-              cuisinesList.map((cuisine) => {
-                return (
-                  <option key={cuisine} value={cuisine}>
-                    {cuisine}
-                  </option>
-                );
-              })}
-          </select>
-          <input
-            id="locationValue"
-            defaultValue={null}
-            placeholder='Location...'
-          />
-          <br />
-          <input
-            id="minPriceValue"
-            defaultValue={null}
-            placeholder='Min price...'
-            type="number" step="0.01"
-          />
-          <input
-            id="maxPriceValue"
-            defaultValue={null}
-            placeholder='Max price...'
-            type="number" step="0.01"
-          />
-          <button className='button' onClick={clearFields} style={{width: '15%', height:'2%'}}>
-            Clear
-          </button>
         </div>
-        <div style={{ width: '100%' }}>
-          <br/>
-          <button className='button' onClick={getData} style={{width: '20%'}}>
-            Search
-          </button>
-        </div>
-      </div>
-      {dishes && dishes.length > 0 && (<div style={{ width: '100%', alignContent: 'center' }}>
-        <Grid2 container spacing={2}>
-          {dishes.map((dish) => (
-            <Link to={`/student/dishes/${dish._id}`}>
-              <Grid2 dish key={dish._id}>
-                <Card>
-                  {/* <CardMedia
-                  component="img"
-                  height="140"
-                  image={item.image}
-                  alt={item.title}
-                /> */}
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {dish.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {dish.description}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      CUISINE: {dish.cuisineType}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      COST: {dish.cost} $
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid2>
-            </Link>
-          ))}
-        </Grid2>
-        <br />
-      </div>
       )
       } {
         dishes && dishes.length == 0 && <div>
