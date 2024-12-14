4acb761683271433b049bf0be50364e31879e01f
SohamX <sohamtalekar7@gmail.com>
2024-12-14 17:07:30 -0500

    made search bar look good

--- Client/src/components/Student/Student.jsx
+++ Client/src/components/Student/Student.jsx
@@ -1,14 +1,36 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useApi } from "../../contexts/ApiContext";
 import { AuthContext } from "../../contexts/AccountContext";
-import { AppBar, Toolbar, Typography, Button, Box, IconButton, InputBase, Card, CardContent, CardMedia } from '@mui/material';
-import backgroundImage from './../../assets/Background_1.png';
-import { Link } from 'react-router-dom';
-import { TextField, Container, Grid2, Select } from "@mui/material";
-import InputAdornment from '@mui/material/InputAdornment';
+import { Typography, Button, Box, Card, CardContent, TextField, Container, Grid2, Avatar, Select, Popper, Paper, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import ClearIcon from '@mui/icons-material/Clear';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import axios from "axios";
+
+  //LIST OF CUISINES
+  const cuisinesList = [
+    { name: 'AMERICAN', icon: '🍔' },
+    { name: 'MEXICAN', icon: '🌮' },
+    { name: 'ITALIAN', icon: '🍝' },
+    { name: 'FRENCH', icon: '🥖' },
+    { name: 'GREEK', icon: '🥙' },
+    { name: 'DUTCH', icon: '🥞' },
+    { name: 'POLISH', icon: '🥟' },
+    { name: 'HUNGARIAN', icon: '🍲' },
+    { name: 'INDIAN', icon: '🍛' },
+    { name: 'CHINESE', icon: '🥡' },
+    { name: 'JAPANESE', icon: '🍣' },
+    { name: 'KOREAN', icon: '🍜' },
+    { name: 'VIETNAMESE', icon: '🍜' },
+    { name: 'THAI', icon: '🍲' },
+    { name: 'MONGOLIAN', icon: '🍲' },
+    { name: 'OTHER', icon: '🍽️' },
+    { name: 'ALL', icon: '🍽️' }
+  ]
 
 const student = () => {
   const { apiCall, loading, error } = useApi();
@@ -17,47 +39,35 @@ const student = () => {
   const [dishes, setDishes] = useState(null)
   const [openModal, setOpenModal] = useState(false)
   const [loading1, setLoading] = useState(true);
-
-  //LIST OF CUISINES
-  const cuisinesList = [
-    'AMERICAN',
-    'MEXICAN',
-    'ITALIAN',
-    'FRENCH',
-    'GREEK',
-    'DUTCH',
-    'POLISH',
-    'HUNGARIAN',
-    'INDIAN',
-    'CHINESE',
-    'JAPANESE',
-    'KOREAN',
-    'VIETNAMESE',
-    'THAI',
-    'MONGOLIAN',
-    'OTHER',
-    'ALL'
-  ]
-
-  //FIELDS TO SEARCH
   const [searchName, setName] = useState('')
+  const [locationAnchorEl, setLocationAnchorEl] = useState(null);
+  const [priceAnchorEl, setPriceAnchorEl] = useState(null);
+  const [location, setLocation] = useState('');
+  const [minPrice, setMinPrice] = useState(undefined);
+  const [maxPrice, setMaxPrice] = useState(undefined);
 
   const clearFields = async () => {
     document.getElementById('nameValue').value = ''
-    document.getElementById('cuisineValue').value = ''
-    document.getElementById('locationValue').value = ''
-    document.getElementById('minPriceValue').value = ''
-    document.getElementById('maxPriceValue').value = ''
+    // document.getElementById('locationValue').value = ''
+    // document.getElementById('minPriceValue').value = ''
+    // document.getElementById('maxPriceValue').value = ''
+    setDishes(null)
+    setLocation('')
+    setMinPrice(null)
+    setMaxPrice(null)
+    setLocationAnchorEl(null)
+    setPriceAnchorEl(null)
     setName('')
   }
 
   // useEffect(()=> {
   async function getData() {
     let dishName = document.getElementById('nameValue').value
-    let cuisineName = document.getElementById('cuisineValue').value
-    let locationName = document.getElementById('locationValue').value
-    let minPrice = document.getElementById('minPriceValue').value
-    let maxPrice = document.getElementById('maxPriceValue').value
+    let cuisineName = searchName
+    // let locationName = document.getElementById('locationValue').value
+    // let minPrice = document.getElementById('minPriceValue').value
+    // let maxPrice = document.getElementById('maxPriceValue').value
+    let locationName = location
 
     const validNumberRegex = /^\d+(\.\d{1,2})?$/;
     if (minPrice && !validNumberRegex.test(minPrice.trim())) {
@@ -108,76 +118,205 @@ const student = () => {
     }
   }, [currentUser]);
 
+  const handleLocationClick = (event) => {
+    setLocationAnchorEl(locationAnchorEl ? null : event.currentTarget);
+    setPriceAnchorEl(null);
+  };
+
+  const handlePriceClick = (event) => {
+    setPriceAnchorEl(priceAnchorEl ? null : event.currentTarget);
+    setLocationAnchorEl(null);
+  };
+  
+  const locationOpen = Boolean(locationAnchorEl);
+  const priceOpen = Boolean(priceAnchorEl);
+
   return (
     <>
-      <Container maxWidth="md" style={{ marginTop: "3%", marginBottom: "4%" }}>
+      <Container maxWidth="md" style={{ marginTop: "3%", marginBottom: "4%", maxWidth: "90vw", justifyItems: 'center' }}>
         <Box sx={{ mt: 4, p: 3, boxShadow: 3, borderRadius: 2 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Search For The Food That You Like...!
-          </Typography>
           <Box mb={4} style={{
             display: 'flex',
-            flexWrap: 'wrap',
+            flexDirection: 'column',
             alignContent: 'space-around',
-            justifyContent: 'space-between'
+            justifyContent: 'space-between',
+            marginBottom: '1rem'
           }}>
-            <TextField
-              id="nameValue"
-              margin="normal"
-              defaultValue={null}
-              placeholder='Food name...'
-              width={{width: 'auto'}}
-            />
-            <Select
-              id="cuisineValue"
-              margin="normal"
-              value={searchName || ''}
-              className='form-control'
-              onChange={(e) => {
-                setName(e.target.value)
-              }}
-              style={{
-                width: '20%',
-                height: '90%',
-                top: '1rem',
+            <Box style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              alignContent: 'space-around',
+              justifyContent: 'space-between'
+            }}>
+              <TextField
+                id="nameValue"
+                margin="normal"
+                defaultValue={null}
+                placeholder='Search For The Food That You Like...!'
+                style={{width: '79%', marginLeft: '1%'}}
+              />
+              {/* <Select
+                id="cuisineValue"
+                margin="normal"
+                value={searchName || ''}
+                className='form-control'
+                onChange={(e) => {
+                  setName(e.target.value)
+                }}
+                style={{
+                  width: '20%',
+                  height: '90%',
+                  top: '1rem',
+                }}
+              >
+                <option value={''} disabled>Select Cuisine</option>
+                {cuisinesList &&
+                  cuisinesList.map((cuisine) => {
+                    return (
+                      <option key={cuisine} value={cuisine}>
+                        {cuisine}
+                      </option>
+                    );
+                  })}
+              </Select> */}
+              {/* <TextField
+                id="locationValue"
+                margin="normal"
+                defaultValue={null}
+                placeholder='Location...'
+                style={{width: 'auto'}}
+              />
+              <br />
+              <TextField
+                id="minPriceValue"
+                margin="normal"
+                defaultValue={null}
+                placeholder='Min price...'
+                type="number" step="0.01"
+                style={{width: "20%"}}
+              />
+              <TextField
+                id="maxPriceValue"
+                margin="normal"
+                defaultValue={null}
+                placeholder='Max price...'
+                type="number" step="0.01"
+                style={{width: "20%"}}
+              /> */}
+              <Box sx={{ display: 'flex', alignItems: 'center', width: '20%', justifyContent: 'space-evenly' }}>
+                <IconButton onClick={handleLocationClick} style={{ width: 'auto' }}>
+                  <LocationOnIcon />
+                </IconButton>
+                <Popper open={locationOpen} anchorEl={locationAnchorEl} placement="bottom-start" style={{ width: '50vw' }}
+                  modifiers={[
+                    {
+                      name: 'preventOverflow',
+                      options: {
+                        boundary: 'viewport',
+                      },
+                    },
+                ]}>
+                  <Paper sx={{ p: 2 }}>
+                    <TextField
+                      id="locationValue"
+                      margin="normal"
+                      value={location}
+                      placeholder='Location...'
+                      style={{ width: '100%' }}
+                      onChange={(e) => setLocation(e.target.value)}
+                    />
+                  </Paper>
+                </Popper>
+                <IconButton onClick={handlePriceClick} style={{ width: 'auto' }}>
+                  <AttachMoneyIcon />
+                </IconButton>
+                <Popper open={priceOpen} anchorEl={priceAnchorEl} placement="bottom-start" style={{ width: '40vw' }}
+                  modifiers={[
+                    {
+                      name: 'preventOverflow',
+                      options: {
+                        boundary: 'viewport',
+                      },
+                    },
+                ]}>
+                  <Paper sx={{ p: 2, display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                    <TextField
+                      id="minPriceValue"
+                      margin="normal"
+                      value={minPrice}
+                      placeholder='Min price...'
+                      type="number"
+                      step="0.01"
+                      style={{ width: "45%" }}
+                      onChange={(e) => setMinPrice(e.target.value)}
+                    />
+                    <TextField
+                      id="maxPriceValue"
+                      margin="normal"
+                      value={maxPrice}
+                      placeholder='Max price...'
+                      type="number"
+                      step="0.01"
+                      style={{ width: "45%" }}
+                      onChange={(e) => setMaxPrice(e.target.value)}
+                    />
+                  </Paper>
+                </Popper>
+                <IconButton onClick={getData} style={{ width: 'auto' }}>
+                  <SearchIcon />
+                </IconButton>
+                <IconButton onClick={clearFields} style={{ width: 'auto' }}>
+                  <ClearIcon />
+                </IconButton>
+              </Box>
+            </Box>
+            {/* <Grid2 container spacing={2} sx={{ mt: 2, display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+              {cuisinesList.map((cuisine) => (
+                <Grid2 item xs={6} sm={4} md={3} key={cuisine.name}>
+                  <Box onClick={() => setName(cuisine.name)} sx={{ textAlign: 'center', cursor: 'pointer', width: "auto" }}>
+                    <Avatar sx={{ width: 56, height: 56, margin: '0 auto' }}>{cuisine.icon}</Avatar>
+                    <Typography variant="body1">{cuisine.name}</Typography>
+                  </Box>
+                </Grid2>
+              ))}
+            </Grid2> */}
+            <Box
+              sx={{
+                mt: 2,
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                overflowX: 'auto',
+                scrollbarWidth: 'none',
+                '&::-webkit-scrollbar': {
+                  width: '0px',
+                  background: 'transparent'
+                },
               }}
             >
-              <option value={''} disabled>Select Cuisine</option>
-              {cuisinesList &&
-                cuisinesList.map((cuisine) => {
-                  return (
-                    <option key={cuisine} value={cuisine}>
-                      {cuisine}
-                    </option>
-                  );
-                })}
-            </Select>
-            <TextField
-              id="locationValue"
-              margin="normal"
-              defaultValue={null}
-              placeholder='Location...'
-              style={{width: 'auto'}}
-            />
-            <br />
-            <TextField
-              id="minPriceValue"
-              margin="normal"
-              defaultValue={null}
-              placeholder='Min price...'
-              type="number" step="0.01"
-              style={{width: "20%"}}
-            />
-            <TextField
-              id="maxPriceValue"
-              margin="normal"
-              defaultValue={null}
-              placeholder='Max price...'
-              type="number" step="0.01"
-              style={{width: "20%"}}
-            />
+              {cuisinesList.map((cuisine) => (
+                <Box
+                  key={cuisine.name}
+                  onClick={() => setName((prev)=> prev === cuisine.name ? '' : cuisine.name)}
+                  sx={{
+                    textAlign: 'center',
+                    cursor: 'pointer',
+                    width: 'auto',
+                    flex: '0 0 auto',
+                    padding: '0 10px',
+                  }}
+                >
+                  {cuisine.name === searchName ? (
+                    <CheckCircleIcon sx={{ width: 56, height: 46, margin: '0 auto', color: 'green' }} />
+                  ) : (
+                    <Avatar sx={{ width: 56, height: 56, margin: '0 auto' }}>{cuisine.icon}</Avatar>
+                  )}
+                  <Typography variant="body1">{cuisine.name}</Typography>
+                </Box>
+              ))}
+            </Box>
           </Box>
-          <Box mb={4}>
+          {/* <Box mb={4}>
             <Button
               fullWidth
               variant="contained"
@@ -196,7 +335,7 @@ const student = () => {
             >
               Search
             </Button>
-          </Box>
+          </Box> */}
       </Box>
       </Container>
       {dishes && dishes.length > 0 && (
