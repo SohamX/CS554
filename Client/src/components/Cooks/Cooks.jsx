1c61b134f9525e78c60cdfc7fedb3fe1645904c6
Sakshi Sherkar <ssherkar1@stevens.edu>
2024-12-18 18:21:26 -0500

    Cooks home page - fix to show cook's accepted meal order

--- Client/src/components/Cooks/Cooks.jsx
+++ Client/src/components/Cooks/Cooks.jsx
@@ -12,28 +12,42 @@ const Cook = () => {
     const navigate = useNavigate();
     const { inOrders } = useContext(SocketContext); // this has incomplete orders
     const [loading, setLoading] = useState(true);
-
+    const [pendingOrders, setOrders] = useState([]);
+    const [pendingRequests, setRequests] = useState([]);
     const [pendingMR, setMR] = useState(null);
 
     useEffect(() => {
         if (!currentUser) {
             navigate("/");
         }
-        async function fetchData() {
-            try {
-                setLoading(true)
-                const pendingMealReqs = await axios.get(`${import.meta.env.VITE_SERVER_URL}/mealReqs/${currentUser._id}/pendingMr`);
-                setMR(pendingMealReqs.data.pendingMealReqs);
-                setLoading(false);
-            } catch (e) {
-                alert(e);
+        // async function fetchData() {
+        //     try {
+        //         setLoading(true)
+        //         const pendingMealReqs = await axios.get(`${import.meta.env.VITE_SERVER_URL}/mealReqs/${currentUser._id}/acceptedMr`);
+        //         setRequests(pendingMealReqs.data.accpetedMealReqs);
+        //         setLoading(false);
+        //     } catch (e) {
+        //         alert(e);
+        //     }
+        // }
+        // fetchData()
+        let order_s = []
+        let requests_s = []
+        if (inOrders) {
+            for (let order of inOrders) {
+                if (order.isMealReq) {
+                    requests_s.push(order)
+                } else {
+                    order_s.push(order)
+                }
             }
         }
-        fetchData()
-    }, [currentUser, navigate]);
-
-    console.log(pendingMR);
-    console.log(inOrders);
+        setOrders(order_s)
+        setRequests(requests_s)
+        // console.log("final 1", order_s);
+        // console.log("final 2", requests_s);
+        setLoading(false)
+    }, [currentUser, navigate, inOrders]);
 
     return (
         <>
@@ -56,14 +70,14 @@ const Cook = () => {
                     height: "100%"
                 }}>
                     {
-                        !loading && inOrders && inOrders.length > 0 &&
+                        !loading && pendingOrders && pendingOrders.length > 0 &&
                         <>
                             <Grid2 width='90%' flex={1} p={2} marginTop={2}>
                                 <Typography variant="h5" component="div">
                                     Upcoming orders
                                 </Typography>
                             </Grid2>
-                            {inOrders.map((order) => (
+                            {pendingOrders.map((order) => (
                                 <Grid2 xs={6} key={order._id} width='90%' justifyContent="center" margin={2} flex={12} p={2}>
                                     <Card>
                                         <CardContent>
@@ -89,7 +103,7 @@ const Cook = () => {
                             )}
                         </>
                     } {
-                        !loading && (!inOrders || inOrders.length === 0) &&
+                        !loading && (!pendingOrders || pendingOrders.length === 0) &&
                         <Grid2 width='90%' flex={1} p={2} marginTop={2}>
                             <Grid2 width='90%'>
                                 <Typography variant="h5" component="div">
@@ -111,35 +125,32 @@ const Cook = () => {
                     overflowY: "auto",
                     height: "100%"
                 }}>
-                    {!loading && pendingMR && pendingMR.length > 0 &&
+                    {!loading && pendingRequests && pendingRequests.length > 0 &&
                         <>
                             <Grid2 width='90%' flex={1} p={2} marginTop={2}>
                                 <Typography variant="h5" component="div">
                                     Meal Requests
                                 </Typography>
                             </Grid2>
-                            {pendingMR.map((mealReq) => (
+                            {pendingRequests.map((mealReq) => (
                                 <Grid2 item xs={6} key={mealReq._id} width='90%' justifyContent="center" margin={2} flex={12} p={2}>
                                     <Card>
                                         <CardContent>
                                             <Typography variant="subtitle1" color="textSecondary">
+                                                <Link to={`/cook/orders/${mealReq._id}`}>
                                                 Placed By: <strong>@{mealReq.username}</strong>
+                                                </Link>
                                             </Typography>
                                             <Typography variant="subtitle1" color="textSecondary">
                                                 Placed On: <strong>{new Date(mealReq.createdAt).toLocaleString()}</strong>
                                             </Typography>
-                                            <Typography variant="subtitle1" color="textSecondary">
-                                                Cuisine: <strong>{mealReq.cuisineType}</strong>
-                                            </Typography>
-                                            <Typography variant="subtitle1" color="textSecondary">
-                                                Description: <strong>{mealReq.description}</strong>
-                                            </Typography>
-                                            <Typography variant="subtitle1" color="textSecondary">
-                                                Quantity / For No. of People: <strong>{mealReq.noOfPeople}</strong>
-                                            </Typography>
-                                            <Typography variant="subtitle1" color="textSecondary">
-                                                Required By: <strong>{new Date(mealReq.requiredBy).toLocaleString()}</strong>
-                                            </Typography>
+                                            {
+                                                mealReq.dishes.dishes.map((dish, index) => (
+                                                    <Typography key={index}>
+                                                        {dish.dishName} : {dish.quantity}
+                                                    </Typography>
+                                                ))
+                                            }
                                         </CardContent>
                                     </Card>
                                 </Grid2>
@@ -147,11 +158,11 @@ const Cook = () => {
                             }
                         </>
                     } {
-                        !loading && (!pendingMR || pendingMR.length === 0) &&
+                        !loading && (!pendingRequests || pendingRequests.length === 0) &&
                         <Grid2 width='90%' flex={1} p={2} marginTop={2}>
                             <Grid2 width='90%'>
                                 <Typography variant="h5" component="div">
-                                    Meal Requests
+                                    Upcoming Meal Requests
                                 </Typography>
                             </Grid2>
                             <CheckCircleIcon
@@ -159,7 +170,7 @@ const Cook = () => {
                                 sx={{ fontSize: 50, mb: 1, marginTop: 10 }}
                             />
                             <Typography variant="h5" component="div">
-                                No more meal requests to explore!
+                                You are all caught up with the meal requests!
                             </Typography>
                         </Grid2>
                     }
