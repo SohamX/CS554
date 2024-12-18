554cc21fd3a5e87ee8e3c285f237371e5c06fb0b
Sakshi Sherkar <ssherkar1@stevens.edu>
2024-12-18 14:47:02 -0500

    state drop-down implementation, backend issue fixes

--- Client/src/components/Cooks/CookProfile.jsx
+++ Client/src/components/Cooks/CookProfile.jsx
@@ -2,10 +2,23 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useApi } from "../../contexts/ApiContext";
 import { AuthContext } from "../../contexts/AccountContext";
-import { Container, Typography, TextField, Button, Box, Grid2, Avatar, FormControlLabel, Switch } from "@mui/material";
+import { Container, Typography, TextField, Button, Box, Grid2, Avatar, FormControlLabel, Switch, FormControl, Select, MenuItem } from "@mui/material";
 import { getLocation, getCoordinatesFromAddress, getDistance } from '../../helpers/constants.js';
 
 const CookProfile = () => {
+    const statesFF = [
+        "Alabama - AL", "Alaska - AK", "Arizona - AZ", "Arkansas - AR", "California - CA",
+        "Colorado - CO", "Connecticut - CT", "Delaware - DE", "Florida - FL", "Georgia - GA",
+        "Hawaii - HI", "Idaho - ID", "Illinois - IL", "Indiana - IN", "Iowa - IA",
+        "Kansas - KS", "Kentucky - KY", "Louisiana - LA", "Maine - ME", "Maryland - MD",
+        "Massachusetts - MA", "Michigan - MI", "Minnesota - MN", "Mississippi - MS",
+        "Missouri - MO", "Montana - MT", "Nebraska - NE", "Nevada - NV", "New Hampshire - NH",
+        "New Jersey - NJ", "New Mexico - NM", "New York - NY", "North Carolina - NC",
+        "North Dakota - ND", "Ohio - OH", "Oklahoma - OK", "Oregon - OR", "Pennsylvania - PA",
+        "Rhode Island - RI", "South Carolina - SC", "South Dakota - SD", "Tennessee - TN",
+        "Texas - TX", "Utah - UT", "Vermont - VT", "Virginia - VA", "Washington - WA",
+        "West Virginia - WV", "Wisconsin - WI", "Wyoming - WY"
+    ]
     const { apiCall, loading, error } = useApi();
     const { currentUser, setCurrentUser } = useContext(AuthContext);
     const [editPersonalInfo, setEditPersonalInfo] = useState(false);
@@ -379,7 +392,7 @@ const CookProfile = () => {
                         onChange={handleAddressChange}
                         disabled={!editAddress}
                     />
-                    <TextField
+                    {/* <TextField
                         fullWidth
                         margin="normal"
                         label="State"
@@ -387,7 +400,26 @@ const CookProfile = () => {
                         value={address.state}
                         onChange={handleAddressChange}
                         disabled={!editAddress}
-                    />
+                    /> */}
+                    <FormControl fullWidth margin="normal" required>
+                        <Select
+                            // label="State"
+                            name="state"
+                            value={address.state}
+                            onChange={handleAddressChange}
+                            displayEmpty
+                            disabled={!editAddress}
+                        >
+                            <MenuItem value="" disabled>
+                                Select State
+                            </MenuItem>
+                            {statesFF.map((state) => (
+                                <MenuItem key={state} value={state}>
+                                    {state}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
                     <TextField
                         fullWidth
                         margin="normal"
